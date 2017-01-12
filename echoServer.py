c3b82e4396810069701dba08a91b1e702c18e936
Elliott Fix <elliott.fix@gmail.com>
2017-01-12 01:58:21 -0500

    Update and rename echoServer to echoServer.py

--- echoServer.py
+++ echoServer.py
@@ -2,20 +2,36 @@ import socket
 
 from afxres import AFX_IDC_COLOR_BLUE
  
-def Main():
+def Red():
     host = "127.0.0.1"
     port = 10000
-    
-    #create two sockets, one for each client. 
-    #Two sockets can use the same port and host (I think) 
-    #What is wrong here?
+     
     redSocket = socket.socket()
     redSocket.bind((host,port))
+     
+    redSocket.listen(1)
+    conn, addr = redSocket.accept()
+    print ("Connection from: " + str(addr))
+    while True:
+            data = conn.recv(1024).decode()
+            if not data:
+                    break
+            print ("from connected  user: " + str(data))
+                
+            print ("sending: " + str(data))
+            conn.send(data.encode())
+            
+    conn.close()
+
+def Blue():
+    host = "127.0.0.1"
+    port = 10000
+     
     blueSocket = socket.socket()
     blueSocket.bind((host,port))
      
-    mySocket.listen(1)
-    conn, addr = mySocket.accept()
+    blueSocket.listen(1)
+    conn, addr = blueSocket.accept()
     print ("Connection from: " + str(addr))
     while True:
             data = conn.recv(1024).decode()
@@ -37,4 +53,7 @@ def Main():
     conn.close()
      
 if __name__ == '__main__':
-    Main()
+    Red()
+    
+#if __name__ == '__main__':
+    #Blue()
