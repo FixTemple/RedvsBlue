413a9a462e2d2587f42102076627e8274c6b25cd
Elliott Fix <elliott.fix@gmail.com>
2017-01-30 20:22:54 -0500

    Update echoServer.py

--- echoServer.py
+++ echoServer.py
@@ -1,3 +1,14 @@
+# Programmer: Elliott Fix
+# Temple University, College of Engineering
+# Start Date: December, 2016
+# Version: 0.1
+# Code inspired by Shakeel Osmani
+#        https://shakeelosmani.wordpress.com/2015/04/13/python-3-socket-programming-example/
+# Description: A basic CLient-Server program that features two client GUIs for
+#              two players to compete in a game setting that models the attack
+#              and defense of Internet security.
+
+
 import socket
 
 from afxres import AFX_IDC_COLOR_BLUE
@@ -37,14 +48,7 @@ def Blue():
             data = conn.recv(1024).decode()
             if not data:
                     break
-            print ("from connected  user: " + str(data))
-             
-            if data == "attack":
-                data = "You are red team"
-            elif data == "defense":
-                data = "You are blue team"
-            else:
-                print ("Please enter only attack or defense")    
+            print ("from connected  user: " + str(data))   
                 
             print ("sending: " + str(data))
             conn.send(data.encode())
