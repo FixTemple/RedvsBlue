1a4e793ce98913072bf703c8a70eaccd8eababe7
Elliott Fix <elliott.fix@gmail.com>
2017-04-18 01:17:34 -0400

    Update RedMove.py

--- RedMove.py
+++ RedMove.py
@@ -12,83 +12,81 @@ from tkinter import *
 import tincanchat
 from multiprocessing import Queue
 import queue
+from RedBlueServer import RedBlueServer
 
 
 class RedMove(object):
     pointTotal = 0
-    point13 = 13
-    point12 = 12
-    point11 = 11
-    point23 = 23
-    point22 = 22
-    point21 = 21
-    point33 = 33
-    point32 = 32
-    point31 = 31
-    delayCost = 10.
+    point11 = 20
+    point12 = 40
+    point13 = 50
+    point21 = 10
+    point22 = 20
+    point23 = 50
+    point31 = 20
+    point32 = 40
+    point33 = 50
+    delayCost = 10
+    sendAttackCost = 0
     max_grid_point = 33
 
     
     
     def __init__(self, master, queue, endCommand, sock):        
-        self.color = 'red' 
+        self.redcolor = 'red'
+        self.bluecolor = 'lightblue' 
         self.queue = queue
         height = 4
         width = 12
         self.sock = sock
         self.root=master
-        self.create_winGUI(self.root, self.color, height, width)
+        self.create_winGUI(self.root, self.redcolor, height, width)
 
     
-    def Color13(self):
-        msg ='R-13'
-        tincanchat.send_msg(self.sock, msg)
-        
-          
-    def Color12(self):
-        msg ='R-12'
-        tincanchat.send_msg(self.sock, msg)
-
-        
     def Color11(self):
         msg ='R-11'
-        tincanchat.send_msg(self.sock, msg)
+        self.button_check_send(msg, self.pointTotal, self.point11)
+
+    def Color12(self):
+        msg ='R-12'
+        self.button_check_send(msg, self.pointTotal, self.point12)
+
+    def Color13(self):
+        msg ='R-13'
+        self.button_check_send(msg, self.pointTotal, self.point13)
         
+    def Color21(self):
+        msg ='R-21'
+        self.button_check_send(msg, self.pointTotal, self.point21)
+
+    def Color22(self):
+        msg ='R-22'
+        self.button_check_send(msg, self.pointTotal, self.point22)
         
     def Color23(self):
         msg ='R-23'
-        tincanchat.send_msg(self.sock, msg)
-        
-        
-    def Color22(self):
-        msg ='R-22'
-        tincanchat.send_msg(self.sock, msg)
-        
-              
-    def Color21(self):
-        msg ='R-21'
-        tincanchat.send_msg(self.sock, msg)
-        
-        
-    def Color33(self):
-        msg ='R-33'
-        tincanchat.send_msg(self.sock, msg)
-        
-        
-    def Color32(self):
-        msg ='R-32'
-        tincanchat.send_msg(self.sock, msg)
-        
+        self.button_check_send(msg, self.pointTotal, self.point23)
         
     def Color31(self):
         msg ='R-31'
-        tincanchat.send_msg(self.sock, msg)
+        self.button_check_send(msg, self.pointTotal, self.point31)
+
+    def Color32(self):
+        msg ='R-32'
+        self.button_check_send(msg, self.pointTotal, self.point32)
         
+    def Color33(self):
+        msg ='R-33'
+        self.button_check_send(msg, self.pointTotal, self.point33)
+
         
     def DelayMove(self):
         self.pointTotal = self.move_delay(self.delayCost, self.pointTotal)
         self.label_pointTotal.config(text = "Point Total: " + str(self.pointTotal))
         
+    def SendAttack(self):
+        msg = 'RA'
+        self.button_check_send(msg, self.pointTotal, self.sendAttackCost)
         
     def testReset(self):
         self.pointTotal = 0
@@ -101,16 +99,17 @@ class RedMove(object):
         self.sock.close()       #Closes the socket, disconnects client
     
     
-    def create_winGUI(self, root, color, height, width):
-        self.button13 = Button(root, text = self.point13, command = self.Color13, height=height, width=width, state = DISABLED)
-        self.button12 = Button(root, text = self.point12, command = self.Color12, height=height, width=width, state = DISABLED)
+    def create_winGUI(self, root, redcolor, height, width):
         self.button11 = Button(root, text = self.point11, command = self.Color11, height=height, width=width)
-        self.button23 = Button(root, text = self.point23, command = self.Color23, height=height, width=width, state = DISABLED)
-        self.button22 = Button(root, text = self.point22, command = self.Color22, height=height, width=width, state = DISABLED)
+        self.button12 = Button(root, text = self.point12, command = self.Color12, height=height, width=width, state = DISABLED)
+        self.button13 = Button(root, text = self.point13, command = self.Color13, height=height, width=width, state = DISABLED)
         self.button21 = Button(root, text = self.point21, command = self.Color21, height=height, width=width)
-        self.button33 = Button(root, text = self.point33, command = self.Color33, height=height, width=width, state = DISABLED)
-        self.button32 = Button(root, text = self.point32, command = self.Color32, height=height, width=width, state = DISABLED)
+        self.button22 = Button(root, text = self.point22, command = self.Color22, height=height, width=width, state = DISABLED)
+        self.button23 = Button(root, text = self.point23, command = self.Color23, height=height, width=width, state = DISABLED)
         self.button31 = Button(root, text = self.point31, command = self.Color31, height=height, width=width)
+        self.button32 = Button(root, text = self.point32, command = self.Color32, height=height, width=width, state = DISABLED)
+        self.button33 = Button(root, text = self.point33, command = self.Color33, height=height, width=width, state = DISABLED)
+
         
         self.buttongroup =  {'13': self.button13 , '23': self.button23 , '33': self.button33,
                              '12': self.button12 , '22': self.button22 , '32': self.button32,
@@ -129,9 +128,9 @@ class RedMove(object):
         self.server_textbox = Text(root, height=height, width=3*width, padx=2, pady=2)
        
         #Define buttons
-        self.buttonAttack = Button(root, text = 'Send\nAttack', height=height, width=width, state = DISABLED)
+        self.buttonAttack = Button(root, text = 'Send\nAttack', command = self.SendAttack, height=height, width=width, state = DISABLED)
         self.buttonDelay = Button(root, text = "Delay\nMove", command = self.DelayMove, height=height, width=width)
-        self.buttonQuit = Button(root, text = "Quit", command = self.quit, height=height, width=width, bg=self.color)
+        self.buttonQuit = Button(root, text = "Quit", command = self.quit, height=height, width=width, bg=self.redcolor)
         self.testReset = Button(root, text = "Test\nReset", command = self.testReset, height=height, width=width)
         
         #Align labels and buttons in grid
@@ -142,36 +141,36 @@ class RedMove(object):
         self.label_y2.grid(row=2, column=0)
         self.label_y1.grid(row=3, column=0)
         self.label_pointTotal.grid(columnspan=4)
-        self.label_turn.grid(row=4, columnspan=2)
+        self.label_turn.grid(row=4, column=0)
         self.label_Actions.grid(row=0, column=5)
-        self.server_textbox.grid(row=5, column=1, columnspan=3)
+        self.server_textbox.grid(row=5, column=0, columnspan=3)
         self.server_textbox.insert(INSERT, "From Server: ")
-#        self.server_textbox.configure(state=DISABLED)
-        self.button13.grid(row=1, column=1)
-        self.button12.grid(row=2, column=1)
         self.button11.grid(row=3, column=1)
-        self.button23.grid(row=1, column=2)
-        self.button22.grid(row=2, column=2)
+        self.button12.grid(row=2, column=1)
+        self.button13.grid(row=1, column=1)
         self.button21.grid(row=3, column=2)
-        self.button33.grid(row=1, column=3)
-        self.button32.grid(row=2, column=3)
+        self.button22.grid(row=2, column=2)
+        self.button23.grid(row=1, column=2)
         self.button31.grid(row=3, column=3)
+        self.button32.grid(row=2, column=3)
+        self.button33.grid(row=1, column=3)
         self.buttonAttack.grid(row=1, column=5)
         self.buttonDelay.grid(row=2, column=5)
         self.buttonQuit.grid(row=5, column=5)
         self.testReset.grid(row=5, column=4)
 
- 
+        
+    # Misc and Button Functions
  
     def process_incoming(self):    
         while self.queue.qsize():
             try:
                 msg = self.queue.get(0).strip()
+                cord=msg[-4:-2]
                 print("From Server: " + msg)
                 if (msg[0]=='M' and msg[-1:]=='R'):
                     self.label_turn.configure(text="Blue's turn")
                     self.root.update()
-                    cord=msg[-4:-2]
                     self.redraw(cord)
                     print(msg)
                     
@@ -182,15 +181,22 @@ class RedMove(object):
                 elif (msg[0]=='M' and msg[-2:]=='BX'):
                     self.label_turn.configure(text="Blue's turn")
                     self.root.update()
-                    print("Blocked by Blue")
+                    msg = "Blocked by Blue"
+                    self.buttongroup[cord].configure(bg = self.bluecolor)
+                    self.update_server_textbox(msg)
                     
                 elif (msg[0]=='M' and msg[-2:]=='RX'):
                     self.label_turn.configure(text="Red's turn")
                     self.root.update()
-                    print("Blocked by Red")
+                    msg = "Blocked by Red"
+                    self.buttongroup[cord].configure(bg = self.redcolor)
+                    self.update_server_textbox(msg)
                     
+                elif (msg[0]=='*' and msg[1]=='R'):
+                    self.update_server_textbox(msg[3:32])
+                
                 else:
-                    print(msg)
+                    print("ERROR: Unidentified message")
                 
             except queue.Empty:
                 # just on general principles, although we don't
@@ -198,9 +204,20 @@ class RedMove(object):
                 pass
             
 
+    def button_check_send(self, msg, score, cost):
+        if ( True ): ######### FIX THIS so that points aren't deducted when attempting to momve when it is not your turn
+            message = msg
+            tincanchat.send_msg(self.sock, message)
+            self.pointTotal -= cost
+            self.label_pointTotal.config(text = "Point Total: " + str(self.pointTotal)) 
+        else:
+            msg = "Invalid move; wait your turn!\n"
+            self.update_server_textbox(msg)
+ 
+ 
     def redraw(self, cord):
         print("Current Location: " + cord)
-        self.buttongroup[cord].configure(bg=self.color)
+        self.buttongroup[cord].configure(bg=self.redcolor)
         
         #convert the coordinate to an integer
         cord = int(cord)
@@ -240,16 +257,36 @@ class RedMove(object):
         if (cord+21 <= self.max_grid_point):
             self.buttongroup[str(cord+21)].configure(state = DISABLED)
             
-        #close blocks from previous row
-        ###
+        #close blocks from grid row 3
+        if ( (cord % 10) == 2 ):
+            self.buttongroup['11'].configure(state = DISABLED)
+            self.buttongroup['21'].configure(state = DISABLED)
+            self.buttongroup['31'].configure(state = DISABLED)
         
+        #close blocks from grid row 2    
+        if ( (cord % 10) == 3 ):
+            self.buttongroup['12'].configure(state = DISABLED)
+            self.buttongroup['22'].configure(state = DISABLED)
+            self.buttongroup['32'].configure(state = DISABLED)
+
+        if (cord >= 20):
+            self.buttonAttack.configure(state = NORMAL)
+    
         self.label_pointTotal.config(text = "Point Total: " + str(self.pointTotal))
         
         
+    # Updates the server textbox by clearing text and printing the given message
+    def update_server_textbox(self, msg):
+        self.server_textbox.delete('1.0', END)
+        self.server_textbox.insert(INSERT, "From Server:\n" + msg)
+        self.root.update()
         
+        
+    # Function for the Delay Move button: Waits one turn, costs resources
     def move_delay(self, cost, score):
         if score < cost:
-            var = messagebox.showinfo("Invalid Move", "You do not have enough resources!")
+            msg = "Invalid Move", "You do not have enough resources!"
+            self.update_server_textbox(msg)
         else:
             score = score - cost
             print ("Move Delayed")
@@ -280,7 +317,6 @@ class RedMove(object):
         else:
             score = score - cost
             print (score)
-        
         return score
 
     
